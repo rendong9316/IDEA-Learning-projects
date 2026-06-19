946a5d8675d2c0a431337573a32a29e4fc8b17b0
rendong9316 <rendong9316@163.com>
2026-06-19 19:58:36 +0800

    web第一个阶段性项目框架创建完毕

---
+++ web-first-project/telers-web-management/src/main/java/com/itheima/TelersWebManagementApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelersWebManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelersWebManagementApplication.class, args);
+    }
+
+}
